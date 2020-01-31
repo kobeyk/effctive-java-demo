6cfbcc39e2d4c6db5f46af9a1a1ab08a2518f434
yukun <437704792@qq.com>
2020-01-31 23:41:18 +0800

    代码更新：使用泛型时通过使用限定通配符增加方法的灵活性案例应用-->手写栈

--- src/main/java/com/appleyk/列表优于数组/Demo3.java
+++ src/main/java/com/appleyk/列表优于数组/Demo3.java
@@ -14,9 +14,9 @@ public class Demo3 {
 
     /**泛型擦除*/
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
         list.add("123");
-        //编译错误  , 属于编译前检查，如果编辑后检查，就晚了，因为泛型对象会在编译时擦除原始类型，统一变为Object类型
+        //编译错误  , 属于编译前检查，如果编译后检查，就晚了，因为泛型对象会在编译后擦除原始类型，统一变为Object类型
 //        list.add(123);
     }
 }
